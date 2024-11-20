da8883e22a91e4f4c1664e9acb13837a9b36952f
Gabiss1 <136657181+Gabiss1@users.noreply.github.com>
2024-11-19 21:49:26 -0300

    Add files via upload

--- UC2-Algoritmos/Exercicios/19-11-24/exercicio-07-entrega.js
+++ UC2-Algoritmos/Exercicios/19-11-24/exercicio-07-entrega.js
@@ -1,9 +1,9 @@
 const rl = require('readline-sync')
 
 function dadosUsuario() {
-    const nome = rl.question('Qual seu nome?')
-    const idade = rl.question('Qual sua idade?')
-    const profissao = rl.question('Qual sua profissao?')
+    const nome = rl.question('Qual seu nome? ')
+    const idade = rl.questionInt('Qual sua idade? ')
+    const profissao = rl.question('Qual sua profissao? ')
 
     const pessoa = {
         nome: nome,
--- UC2-Algoritmos/Exercicios/19-11-24/exercicio-08-entrega.js
+++ UC2-Algoritmos/Exercicios/19-11-24/exercicio-08-entrega.js
@@ -1,17 +1,17 @@
-function filmes() {
-    let filme1 = {
-        nome: 'Jurassic Park - O Parque dos Dinossauros',
-        anoLancamento: 1993
-    }
-    let filme2 = {
-        nome: 'Resident Evil 2: Apocalipse',
-        anoLancamento: 2004
-    }
-
-    console.log(`
-O primeiro filme foi lançado antes do segundo? ${filme1.anoLancamento<filme2.anoLancamento}
-O primeiro filme foi lançado no mesmo ano do segundo? ${filme1.anoLancamento===filme2.anoLancamento}
-O primeiro filme foi lançado depois do segundo? ${filme1.anoLancamento>filme2.anoLancamento}`)
+let filme1 = {
+    nome: 'Jurassic Park - O Parque dos Dinossauros',
+    anoLancamento: 1993
+}
+let filme2 = {
+    nome: 'Resident Evil 2: Apocalipse',
+    anoLancamento: 2004
 }
 
-filmes()
+function filmes(filme, filmeDois) {
+    console.log(`
+O primeiro filme foi lançado antes do segundo? ${filme.anoLancamento<filmeDois.anoLancamento}
+O primeiro filme foi lançado no mesmo ano do segundo? ${filme.anoLancamento===filmeDois.anoLancamento}
+O primeiro filme foi lançado depois do segundo? ${filme.anoLancamento>filmeDois.anoLancamento}`)
+}
+
+filmes(filme1, filme2)
