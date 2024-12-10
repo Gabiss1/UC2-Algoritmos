c7811a4f8244ada26b2bef18e689c17516109ed9
Gabiss1 <136657181+Gabiss1@users.noreply.github.com>
2024-12-10 17:27:21 -0300

    Add files via upload

---
+++ UC2-Algoritmos/hemocentro/script.js
@@ -0,0 +1,28 @@
+const rl = require('readline-sync')
+
+const doadores = []
+
+function cadastro() {
+    const nome = rl.question('Digite seu Nome Completo: ')
+    const idade = rl.questionInt('Digite sua Idade: ')
+    const peso = rl.questionInt('Digite seu Peso: ')
+    const tipoSang = rl.question('Digite seu Tipo Sanguineo (A-, B+, AB+, O-, etc.): ')
+    const dataUltDoacao = rl.question('Digite a data da ultima doacao (dd/mm/aaaa): ')
+
+    const cadastrados = {
+        nome: nome,
+        idade: idade,
+        peso: peso,
+        tipoSang: tipoSang.toUpperCase(),
+        dataUltDoacao: new Date (dataUltDoacao)
+    } 
+    doadores.push(cadastrados)
+    
+}
+
+function name(params) {
+    
+}
+
+cadastro()
+console.log(doadores)
