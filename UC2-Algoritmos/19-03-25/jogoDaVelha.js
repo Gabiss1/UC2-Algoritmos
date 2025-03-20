efe7d27658f33346e832c66c5891b5db5ef97b24
Gabiss1 <136657181+Gabiss1@users.noreply.github.com>
2025-03-19 21:59:13 -0300

    Add files via upload

---
+++ UC2-Algoritmos/19-03-25/jogoDaVelha.js
@@ -0,0 +1,11 @@
+// Trabalhando lógica com matrizes
+
+let tabuleiro = [
+    ['','',''], // Células vazias
+    ['','',''],
+    ['','','']
+]
+
+let vez = 'O'
+let jogoAtivo = true // Deixa o jogo ativo (true significa que o jogo ainda está funcionando)
+
